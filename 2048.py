65ec18e7652db9d2dc78de4fb801757cd4470b28
pizo17 <pizopublic@gmail.com>
2022-03-24 19:52:02 +0100

    numgen done

--- 2048.py
+++ 2048.py
@@ -21,7 +21,7 @@ def get_board_size():
 def print_matrix(matrix):
     for row in matrix:
         for elem in row:
-            print("[" + elem + "]", end="")
+            print(f"[{elem}]", end="")
         print()
 
 
@@ -33,29 +33,51 @@ def choose_next_number():
 
 
 
+def choose_random_coordinates(size):
+    coords = []
+    for i in range(2):
+        coords.append(random.randint(0, size))
+    return coords
+
+
+
+def is_position_free(matrix, coords):
+    return matrix[coords[0]][coords[1]] == " "
+
+
+
 def is_board_full(matrix, return_num_of_empty_spaces=False):
-    num_of_empty_spaces = 0
+    num_of_valid_spaces = 0
     for row in matrix:
         for elem in row:
-            if elem == " ":
-                num_of_empty_spaces += 1
+            if elem == " " and num_of_valid_spaces <= 2:
+                num_of_valid_spaces += 1
     if return_num_of_empty_spaces:
-        return num_of_empty_spaces == 0, num_of_empty_spaces
+        return num_of_valid_spaces == 0, num_of_valid_spaces
     else:
-        return num_of_empty_spaces == 0
+        return num_of_valid_spaces == 0
 
 
 
 def add_new_twos_and_fours(matrix):
-    pass
+    is_full, num_of_valid_spaces = is_board_full(matrix, True)
+    if not is_full:
+        for i in range(num_of_valid_spaces - 1):
+            coords = choose_random_coordinates(len(matrix)-1)
+            while not is_position_free(matrix, coords):
+                coords = choose_random_coordinates(len(matrix))
+            matrix[coords[0]][coords[1]] = choose_next_number()
+    return matrix
+            
 
 
 
 def main():
     matrix = init(get_board_size())
     print_matrix(matrix)
-
-
+    matrix = add_new_twos_and_fours(matrix)
+    print("\n")
+    print_matrix(matrix)
 
 
 if __name__ == "__main__":
